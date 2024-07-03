42ed52aee7962b04e0c2fec6beae7eb7c1d2221c
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-07-03 18:14:52 +0900

    react start

--- my-simple-website/src/App.js
+++ my-simple-website/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header className='App-header'>
+      <h1>홈페이지에 오신것을 환영합니다.</h1>
+     </header>
+      <main>
+        <section>
+          <h2>About Us</h2>
+          <p>이 홈페이지는 간단한 리액트 홈페이지입니다.</p>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <p>Email : contact@email.com</p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2024 My website</p>
+      </footer>
     </div>
   );
 }
